fb0fe0b4c5e64c1889266074cb67134bcdc4543d
Keisha Vu <129679836+keishavu@users.noreply.github.com>
2025-01-08 12:37:11 -0500

    Update todolist.java

--- todolist.java
+++ todolist.java
@@ -6,10 +6,11 @@ private static ArrayList<String> todo = new ArrayList<>();
 private static String operation;
 private static String yn;
 	public static void main(String[] args) {
-
+// prompts user 
 System.out.println("Welcome to To Do List! Use this program to view, add, delete, and update tasks :)");
 System.out.print("What would you like to do with your todo list? Type 'view', 'add', 'delete', 'update', or 'exit' ");
 operation = input.nextLine();
+// while loop that executes when exit is not called 
 while(true) {
 	switch (operation.toLowerCase()) {
 	case "view" -> view();
@@ -25,6 +26,7 @@ while(true) {
 	linebreak();
 	}
 }
+// method to view items in the list 
 private static void view() {
 	if (todo.isEmpty()) {
 		System.out.println("There are not tasks in the queue. ");
@@ -35,23 +37,11 @@ private static void view() {
 			System.out.println(i+1 + ". "+ todo.get(i));
 		}
 	}
-	System.out.print("Would you like to perform another operation? ( yes or no )");
-	yn = input.nextLine();
-	while (true) {
-	if (yn.equalsIgnoreCase("yes")) {
-		System.out.print("What would you like to do next? ");
-		operation = input.nextLine();
-		break;
-	}
-	else if (yn.equalsIgnoreCase("no")) {
-		break;
-	}
-	else {
-		System.out.println("Please type 'yes' or 'no'");
-		yn = input.nextLine();
-	}
-	}
+	System.out.print("Enter a new operation: ");
+	operation = input.nextLine();
+	return;
 }
+//method that allows user to add items to the list 
 private static void add() {
 	while (true) {
 		System.out.print("Enter task: ");
@@ -63,66 +53,74 @@ private static void add() {
 		if (!yn.equalsIgnoreCase("yes")) {
 			System.out.print("Enter the command you would like to continue with: ");
 			operation = input.nextLine();
-			break;
+			return;
 		}
 	}
 }
+// method that allows user to delete items in list 
 private static void delete() {
-	String yn;
-	if (todo.isEmpty()) {System.out.println("Your to do list is empty");
+if (todo.isEmpty()) {
+	System.out.println("Your to do list is empty");
 	System.out.print("Would you like to add tasks to your todo list? ( yes or no )");
 	yn = input.nextLine();
 	while (true) {
 		if (yn.equalsIgnoreCase("yes")) {
 			add();
-			break;
+			return;
 		}
 		else if (yn.equalsIgnoreCase("no")) {
-			break; 
+			System.out.print("Enter a new operation: ");
+			operation = input.nextLine();
+			return; 
 		}
 		else {
 			System.out.println("please enter 'yes' or 'no' ");
 			yn = input.nextLine();
-			}
 		}
 	}
-	else {
-		System.out.println("Here are the tasks currently in your list: ");
-		for (int i = 0; i<todo.size(); i++) {
-			System.out.println(i+1 + ". "+todo.get(i));
-		}
-		System.out.print("What task would you like to delete? Enter number: ");
-		int number = input.nextInt();
-		if (number > 0 && number <= todo.size()) {
-			System.out.println("This is the task that will be deleted: " + todo.get(number -1));
-			System.out.print("Are you sure you want to delete this task? ( yes or no ) : ");
-			yn = input.nextLine();
-			while (true) {
+}
+else {
+	System.out.println("Here are the tasks currently in your list: ");
+	for (int i = 0; i<todo.size(); i++) {
+		System.out.println(i+1 + ". "+todo.get(i));
+	}
+	System.out.print("What task would you like to delete? Enter number: ");
+	int number = 0;
+	if (input.hasNextInt()) {
+		number = input.nextInt();
+		input.nextLine();
+	}
+	if (number > 0 && number <= todo.size()) {
+		System.out.println("This is the task that will be deleted: " + todo.get(number -1));
+		System.out.print("Are you sure you want to delete this task? ( yes or no ) : ");
+		yn = input.nextLine();
+		while (true) {
+			if (yn.equalsIgnoreCase("yes")) {
+				todo.remove(number-1);
+				return;
+			}
+			else if (yn.equalsIgnoreCase("no")) {
+				System.out.print("Would you like to delete another task? ( yes or no ): ");
+				yn = input.nextLine();
 				if (yn.equalsIgnoreCase("yes")) {
-					todo.remove(number-1);
-					break;
+					delete();
+					return;
 				}
 				else if (yn.equalsIgnoreCase("no")) {
-					System.out.print("Would you like to delete another task? ( yes or no ): ");
-					yn = input.nextLine();
-					if (yn.equalsIgnoreCase("yes")) {
-						delete();
-						break;
-					}
-					else if (yn.equalsIgnoreCase("no")) {
-						System.out.println("Enter a new operation: ");
-						operation = input.nextLine();
-						break;
-					}
+					System.out.println("Enter a new operation: ");
+					operation = input.nextLine();
+					return;
 				}
-				else {
-					System.out.println("please enter yes or no: ");
-					yn = input.nextLine();
-				}
+			}
+			else {
+				System.out.println("please enter yes or no: ");
+				yn = input.nextLine();
 			}
 		}
 	}
 }
+}
+// method that allows user to change existing items 
 private static void update() {
 	if(todo.isEmpty()) {
 		System.out.print("Your to do list is empty. Returning back to main menu. Enter next command: ");
